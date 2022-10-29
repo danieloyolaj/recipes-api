a5ecb2b1ae37427132f045f09edf8acaa7f5db95
Daniel Oyola <danieloyola@softerdo.com>
2022-10-28 22:43:08 -0400

    Created categories controllers

---
+++ src/categories/categories.controllers.js
@@ -0,0 +1,32 @@
+const Categories = require('../models/categories.models')
+
+//Controller to get all the categories
+const getAllCategories = async() => {
+  const data = await Categories.findAll()
+  return data
+}
+
+//Controller to get one specific category
+const getCategoryById = async (id) => {
+  const data = await Categories.findOne({ where: {id}})
+  return data
+}
+
+//Controller to create a category
+const createCategory = async (title) => {
+  const data = await Categories.create({title})
+  return data
+}
+
+//Controller to delete a category
+const deleteCategory = async (id) => {
+  const data = await Categories.destroy({where: {id}})
+  return data
+}
+
+module.exports = {
+  getAllCategories,
+  getCategoryById,
+  createCategory,
+  deleteCategory
+}
