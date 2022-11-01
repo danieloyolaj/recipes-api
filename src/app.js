9c0d2eb5b73babcb361e8a6e751ae8d86a06b581
Daniel Oyola <danieloyola@softerdo.com>
2022-10-31 23:31:43 -0400

    Added routes to app.js

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const db = require('./utils/database')
 
+
 //? Files
 const {port} = require('./config');
 //* Routes
 const userRouter = require('./users/users.router')
 const authRouter = require('./auth/auth.router')
 const categoryRouter = require('./categories/categories.router')
+const recipesRouter = require('./recipes/recipes.router')
 const initModels = require('./models/initModels')
 
 //? Initial Configs
@@ -44,6 +46,8 @@ app.get('/',(req, res) => {
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/categories', categoryRouter)
+app.use('/api/v1/recipes', recipesRouter)
+
 
 app.listen(port, () => {
     console.log(`Server started at port ${port}`)
