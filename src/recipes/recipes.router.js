3ed9d95a486d3ade36a0440d91506dbad74ffafa
Daniel Oyola <danieloyola@softerdo.com>
2022-10-31 23:25:09 -0400

    Added recipes router

--- src/recipes/recipes.router.js
+++ src/recipes/recipes.router.js
@@ -1,2 +1,18 @@
+const router = require('express').Router()
 const recipesServices = require('./recipes.services')
+const passport = require('passport')
+require('../middlewares/auth.middleware')(passport)
 
+// /recipes
+// /recipes/:recipes_id
+
+router.route('/')
+  .get(recipesServices.getAllRecipes)
+  .post(passport.authenticate('jwt', {session: false}), recipesServices.createRecipe)
+
+router.route('/:recipe_id')
+  .get(recipesServices.getRecipeById)
+  .patch(passport.authenticate('jwt', {session: false}), recipesServices.patchRecipe)
+  .delete(passport.authenticate('jwt', {session: false}), recipesServices.deleteRecipe)
+
+module.exports = router
